a83231c340c7bc73225fc5b22c986c28db7fa71a
Tarao84 <taraoq@gmail.com>
2016-11-03 21:09:29 +0100

    Update about.php

--- app/presentation/about.php
+++ app/presentation/about.php
@@ -2,7 +2,9 @@
 set_page_subtitle("Turn left and check out these awesome stats.");
 include("header.php");
 ?>
-<h3>À propos de ce site</h3>
+<div class="question-content">
+<div class="question-box">
+  <span class="question-block">À propos de ce site</span>
 <h4>LANGUES OFFICIELLES</h4>
 <p>
 La WFTDA publie les règles en diverses langues. En cas de divergence sur la formulation d’une version en langue étrangère des Règles du Flat Track Roller Derby, le <a href="https://wftda.com/rules" target="blank">texte anglais</a> a autorité.
@@ -19,8 +21,8 @@ Le neutre n’existant pas en français, les termes neutres anglais destinés à
 <p>
 Certains termes propres au Roller Derby ont été laissés en anglais dans le même but de compréhension du jeu car ces termes sont employés en match dans leur langue d’origine.
 </p>
-
-
+  </div>
+</div>
 <?php 
 include("footer.php");
 ?>
