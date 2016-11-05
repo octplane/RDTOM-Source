522382143449cabede57f017c3d6826ea8afb7ac
Pierre Baillet <pierre@baillet.name>
2016-11-05 16:33:08 +0100

    Deploy script for Gandi

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ set -e
 set -x
 
 test -f deploy_key && \
-  ssh-agent bash -c 'ssh-add deploy_key; git pull' || git pull
+  ssh-agent bash -c "ssh-add deploy_key; git pull;git push gandi ssh 7957567@git.dc0.gpaas.net 'deploy www.rollerderbytestomatic.fr'" || git pull
 
