fa9d23a4ef09c11772223a12e660858bcc25843e
Tarao84 <taraoq@gmail.com>
2017-11-14 21:21:01 +0100

    update https://rules.wftda.com/

--- app/presentation/about.php
+++ app/presentation/about.php
@@ -7,7 +7,7 @@ include("header.php");
   <span class="question-block">À propos de ce site</span>
 <h4>LANGUES OFFICIELLES</h4>
 <p>
-La WFTDA publie les règles en diverses langues. En cas de divergence sur la formulation d’une version en langue étrangère des Règles du Flat Track Roller Derby, le <a href="https://wftda.org/rules" target="blank">texte anglais</a> a autorité.
+La WFTDA publie les règles en diverses langues. En cas de divergence sur la formulation d’une version en langue étrangère des Règles du Flat Track Roller Derby, le <a href="https://rules.wftda.com/" target="blank">texte anglais</a> a autorité.
 </p>
 
 <h4>MESURES</h4>
@@ -16,7 +16,7 @@ Les dimensions indiquées dans le système métrique sont arrondies et dès lors
 
 <h4>NOTE À L’ATTENTION DES LECTEURS FRANCOPHONES</h4>
 <p>
-Le neutre n’existant pas en français, les termes neutres anglais destinés à inclure à la fois des éléments masculins et féminins ont été traduits selon la règle de base du français où le masculin s’impose au niveau du genre, afin de faciliter la traduction, la lecture et la compréhension.
+Le neutre n’existant pas en français, les termes neutres anglais destinés à inclure à la fois des éléments masculins et féminins ont été traduits selon la règle de base du français afin de faciliter la traduction, la lecture et la compréhension.
 </p>
 <p>
 Certains termes propres au Roller Derby ont été laissés en anglais dans le même but de compréhension du jeu car ces termes sont employés en match dans leur langue d’origine.
